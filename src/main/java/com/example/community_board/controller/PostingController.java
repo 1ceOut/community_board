589766dff28ae926b18518ad6cf18ef6b081f4a0
Midichi <43335417+8282qwe@users.noreply.github.com>
2024-08-26 21:36:23 +0900

    Update PostingController.java

--- src/main/java/com/example/community_board/controller/PostingController.java
+++ src/main/java/com/example/community_board/controller/PostingController.java
@@ -13,7 +13,6 @@ import java.util.Objects;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = {"http://localhost:8080","https://reacticeout.icebuckwheat.kro.kr"})
 public class PostingController {
 
     @Autowired
